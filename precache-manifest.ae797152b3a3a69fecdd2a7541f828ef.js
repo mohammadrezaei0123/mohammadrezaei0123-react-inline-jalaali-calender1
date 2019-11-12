0c1be7de2cbe2eb8eaf0145fe482459fca2f6425
mohammadrezaei012345 <mohammadrezaei012345@yahoo.com>
2019-11-11 21:30:57 -0800

    Updates

---
+++ precache-manifest.ae797152b3a3a69fecdd2a7541f828ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990ba7134441d3d0a6e9eeffe60aa5e0",
+    "url": "/react-inline-jalaali-calender1/index.html"
+  },
+  {
+    "revision": "65dff2203f6dbcbaad98",
+    "url": "/react-inline-jalaali-calender1/static/css/2.b8446803.chunk.css"
+  },
+  {
+    "revision": "4df59ddc648a7b9bea1d",
+    "url": "/react-inline-jalaali-calender1/static/css/main.9d8cf666.chunk.css"
+  },
+  {
+    "revision": "65dff2203f6dbcbaad98",
+    "url": "/react-inline-jalaali-calender1/static/js/2.04a6b1f6.chunk.js"
+  },
+  {
+    "revision": "4df59ddc648a7b9bea1d",
+    "url": "/react-inline-jalaali-calender1/static/js/main.18c88591.chunk.js"
+  },
+  {
+    "revision": "5b03e242d849018fa791",
+    "url": "/react-inline-jalaali-calender1/static/js/runtime~main.bfbdb805.js"
+  },
+  {
+    "revision": "2be5d53bd9404008e505c403b2af6d9c",
+    "url": "/react-inline-jalaali-calender1/static/media/BKoodkBd_4.2be5d53b.ttf"
+  },
+  {
+    "revision": "2426ea04df6a5935011c714a2c5966f9",
+    "url": "/react-inline-jalaali-calender1/static/media/BMehrBd.2426ea04.ttf"
+  },
+  {
+    "revision": "3504e01ac5160162f8c73a33ab89874a",
+    "url": "/react-inline-jalaali-calender1/static/media/BNaznnBd.3504e01a.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-inline-jalaali-calender1/static/media/logo.5d5d9eef.svg"
+  }
+]);
